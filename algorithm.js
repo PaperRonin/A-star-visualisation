7a2437f7db388b34d41e1b6a0c6b7d3f0df955f1
PaperRonin <goowillprevail@gmail.com>
2020-05-26 16:36:31 +0700

    Implement A-star pathfinding algorithm

--- algorithm.js
+++ algorithm.js
@@ -1,3 +1,72 @@
-function startSearch(){
-inProgress = true;
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+let correction = 0.000001;
+
+function heuristic(a, b) {
+  correction += 0.000001;
+  return Math.abs(a.x - b.x) + Math.abs(a.y - b.y) - correction;
+}
+
+function neighbors(node) {
+  let neighborsArr = [];
+  if (node.x > 0) {
+    neighborsArr.push(map[node.x - 1][node.y]);
+  }
+  if (node.x < map.length - 1) {
+    neighborsArr.push(map[node.x + 1][node.y]);
+  }
+  if (node.y > 0) {
+    neighborsArr.push(map[node.x][node.y - 1]);
+  }
+  if (node.y < map.length - 1) {
+    neighborsArr.push(map[node.x][node.y + 1]);
+  }
+  return neighborsArr;
+}
+
+function AStarSearch(start, goal) {
+  frontier = new PriorityQueue();
+  frontier.push(start);
+  start.cost_so_far = 0;
+  let count = 0;
+  while (!frontier.isEmpty() && count < 900) {
+    current = frontier.pop();
+    count++;
+
+    if (current.type === 'goal') {
+      while (!frontier.isEmpty()) {
+        frontier.pop().type = 'enqueued';
+      }
+      current = current.came_from;
+      while (current.came_from !== null) {
+        current.type = 'path';
+        current = current.came_from; 
+      }
+      start.type = 'start';
+      draw();
+      return current.cost_so_far;
+    }
+
+    // console.log("visiting " + current.x + ":" + current.y);
+    // console.log("p " + current.priority);
+    neighbors(current).forEach(next => {
+      if (next.type !== 'visited' && next.type !== 'wall') {
+        new_cost = current.cost_so_far + next.moveCost;
+        if (next.cost_so_far === null || new_cost < next.cost_so_far) {
+          next.cost_so_far = new_cost;
+          next.priority = new_cost + heuristic(goal, next);
+          // console.log("push " +  next.x + ":" + next.y + " p: " +next.priority);
+          frontier.push(next);
+          next.came_from = current;
+        }
+      }
+    });
+    current.type = 'visited';
+  }
+  return -1;
 }
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let main = document.querySelector(".main");
 
-let PaintColor = "none";
-let endpoint, startPoint;
-var inProgress = false;
+let PaintColor = "green";
+let goalPoint, startPoint;
+let inProgress = false;
 
 Array.matrix = function(numrows, numcols, initial) {
   var arr = [];
@@ -23,11 +23,14 @@ function Point(x, y) {
   this.y = y;
 }
 
-function Node(x, y, type, value) {
+function Node(x, y, type, moveCost) {
   this.x = x;
   this.y = y;
   this.type = type;
-  this.value = value;
+  this.moveCost = moveCost;
+  this.cost_so_far = null;
+  this.came_from = null;
+  this.priority = 0;
 }
 
 function changeColor(element, initialBG) {
@@ -42,6 +45,7 @@ function changeColor(element, initialBG) {
 }
 
 function setPaintColor(color) {
+  document.getElementById('brush').style.background = color;
   PaintColor = color;
 }
 
@@ -49,23 +53,22 @@ function mark(cell, x, y) {
   if (inProgress) {
     return
   }
-  if (PaintColor === "green" && map[x][y].type !== 'end') {
+  if (PaintColor === "green" && map[x][y].type !== 'goal') {
 
-    map[startPoint.x][startPoint.y].type = 'path';
+    map[startPoint.x][startPoint.y].type = 'not visited';
     startPoint = new Point(x, y);
     map[x][y].type = 'start';
-    console.log("done");
   } else if (PaintColor === "red" && map[x][y].type !== 'start') {
 
-    map[endPoint.x][endPoint.y].type = 'path';
-    endPoint = new Point(x, y);
-    map[x][y].type = 'end';
+    map[goalPoint.x][goalPoint.y].type = 'not visited';
+    goalPoint = new Point(x, y);
+    map[x][y].type = 'goal';
 
   } else if (PaintColor === "grey") {
 
     if (map[x][y].type === 'wall') {
-      map[x][y].type = 'path';
-    } else {
+      map[x][y].type = 'not visited';
+    } else if (map[x][y].type !== 'goal' && map[x][y].type !== 'start') {
       map[x][y].type = 'wall';
     }
 
@@ -79,11 +82,15 @@ function draw() {
     for (let x = 0; x < map[y].length; x++) {
       if (map[x][y].type === 'visited') {
         mainInnerHTML += '<div class="cell" style="background: #9ef3c0" onclick="mark(this,' + x + ',' + y + ')"></div>';
+      } else if (map[x][y].type === 'enqueued') {
+        mainInnerHTML += '<div class="cell" style="background:  #5da1ff" onclick="mark(this,' + x + ',' + y + ')"></div>';
+      } else if (map[x][y].type === 'path') {
+        mainInnerHTML += '<div class="cell" style="background: #ffac4d" onclick="mark(this,' + x + ',' + y + ')"></div>';
       } else if (map[x][y].type === 'wall') {
         mainInnerHTML += '<div class="cell" style="background: grey" onclick="mark(this,' + x + ',' + y + ')"></div>';
       } else if (map[x][y].type === 'start') {
         mainInnerHTML += '<div class="cell" style="background: green" onclick="mark(this,' + x + ',' + y + ')"></div>';
-      } else if (map[x][y].type === 'end') {
+      } else if (map[x][y].type === 'goal') {
         mainInnerHTML += '<div class="cell" style="background: red" onclick="mark(this,' + x + ',' + y + ')"></div>';
       } else {
         mainInnerHTML += '<div class="cell" onclick="mark(this,' + x + ',' + y + ')"></div>';
@@ -92,8 +99,37 @@ function draw() {
   }
   main.innerHTML = mainInnerHTML;
 }
+
+async function startSearch() {
+  inProgress = true;
+  AStarSearch(map[startPoint.x][startPoint.y], map[goalPoint.x][goalPoint.y]);
+  let button = document.getElementById('startBttn');
+  button.innerHTML = 'Clear';
+  button.setAttribute("onClick", "clearResult(1);");
+}
+
+async function clearResult(initial) {
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[x][y].type === 'start') {
+        map[x][y] = new Node(x, y, 'start', initial);
+      } else if (map[x][y].type === 'goal') {
+        map[x][y] = new Node(x, y, 'goal', initial);
+      } else if (map[x][y].type !== 'wall') {
+        map[x][y] = new Node(x, y, 'not visited', initial);
+      }
+    }
+  }
+  draw();
+  let button = document.getElementById('startBttn');
+  button.innerHTML = 'Start';
+  button.setAttribute("onClick", "startSearch();");
+    inProgress = false;
+}
+
+
 map[0][0].type = 'start';
-map[6][6].type = 'end';
+map[6][6].type = 'goal';
 startPoint = new Point(0, 0);
-endPoint = new Point(6, 6);
+goalPoint = new Point(6, 6);
 draw();
